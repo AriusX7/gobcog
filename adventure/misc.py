2db73ca633869d4b2c4e4725d9ab24afad0c5d38
AriusX7 <icyligii@gmail.com>
2020-12-10 12:33:11 +0530

    Remove redundant "equipped" from loot msg

--- adventure/misc.py
+++ adventure/misc.py
@@ -2443,10 +2443,13 @@ class MiscMixin(commands.Cog):
                 )
             )
             return None
+
+        # Just in case old_items is empty.
+        slot = item.slot[0]
         old_items = [(i, getattr(character, i, None)) for i in item.slot]
         old_stats = ""
 
-        for slot, old_item in old_items:
+        for num, (slot, old_item) in enumerate(old_items):
             if old_item:
                 old_slot = old_item.slot[0]
                 if len(old_item.slot) > 1:
@@ -2463,29 +2466,42 @@ class MiscMixin(commands.Cog):
                     luck = old_item.luck
                     dex = old_item.dex
 
-                old_stats += (
-                    _("You currently have {item} [{slot}] equipped | Lvl req {lv} equipped.").format(
-                        item=old_item, slot=old_slot, lv=equip_level(character, old_item)
+                    if num == 0:
+                        old_stats += _("You currently have {item} [{slot}] | Lvl req {lv}").format(
+                            item=old_item, slot=old_slot, lv=equip_level(character, old_item)
+                        )
+                        if len(old_items) == 1:
+                            old_stats += " equipped."
+                        else:
+                            old_stats += "."
+                    else:
+                        # we can put equipped here because `num` be only `0` or `1`.
+                        # might have to change this if that changes.
+                        old_stats += _("and {item} [{slot}] | Lvl req {lv} equipped.").format(
+                            item=old_item, slot=old_slot, lv=equip_level(character, old_item)
+                        )
+
+                    old_stats += (
+                        f" (ATT: {str(att)}, "
+                        f"CHA: {str(cha)}, "
+                        f"INT: {str(intel)}, "
+                        f"DEX: {str(dex)}, "
+                        f"LUCK: {str(luck)})\n"
                     )
-                    + f" (ATT: {str(att)}, "
-                    f"CHA: {str(cha)}, "
-                    f"INT: {str(intel)}, "
-                    f"DEX: {str(dex)}, "
-                    f"LUCK: {str(luck)})\n"
-                )
-            if len(item.slot) > 1:
-                slot = _("two handed")
-                att = item.att * 2
-                cha = item.cha * 2
-                intel = item.int * 2
-                luck = item.luck * 2
-                dex = item.dex * 2
-            else:
-                att = item.att
-                cha = item.cha
-                intel = item.int
-                luck = item.luck
-                dex = item.dex
+
+        if len(item.slot) > 1:
+            slot = _("two handed")
+            att = item.att * 2
+            cha = item.cha * 2
+            intel = item.int * 2
+            luck = item.luck * 2
+            dex = item.dex * 2
+        else:
+            att = item.att
+            cha = item.cha
+            intel = item.int
+            luck = item.luck
+            dex = item.dex
 
         equip_lvl = equip_level(character, item)
         if character.lvl < equip_lvl:
