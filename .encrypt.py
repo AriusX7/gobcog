1ec5a62b9aaf1c21c77ca91708b17c200f1d8ad4
AriusX7 <icyligii@gmail.com>
2021-06-09 00:05:12 +0530

    Allow ecrypting a single file

--- .encrypt.py
+++ .encrypt.py
@@ -1,28 +1,16 @@
 import os
+import sys
 from glob import iglob
 
 from cryptography.fernet import Fernet
 
 
-print('This script encrypts all decrypted datafiles from decrypted/*/*.json.')
-print('This will overwrite all pre-existing datafiles saved in adventure/data.')
-print('This is only necessary to edit the datafiles.')
-print('IMPORTANT: Take extra caution not to commit decrypted datafiles.')
-confirm = input('Reply with Y to confirm: ')
-
-if confirm != 'Y':
-    exit(0)
-
-
-for i in iglob('decrypted/*/*.json'):
-    theme = os.path.normpath(i).split(os.sep)[1]
-    name = os.path.split(i)[1]
-
+def encrpyt_file(path, name, theme):
     with open(f'decrypted/{theme}/key.key', 'rb') as f:
         key_data = f.read()
         key = Fernet(key_data)
 
-    with open(i, 'rb') as f:
+    with open(path, 'rb') as f:
         encrypted_data = key.encrypt(f.read())
 
     # create theme folder if doesnt exist
@@ -37,4 +25,31 @@ for i in iglob('decrypted/*/*.json'):
     with open(f'adventure/data/{theme}/{name[:-5]}.enc', 'wb') as f:
         f.write(encrypted_data)
 
-    print(name)
+
+path = sys.argv[1] if len(sys.argv) > 1 else None
+
+if path and os.path.exists(path):
+    import pathlib
+
+    path = pathlib.PurePath(path)
+    encrpyt_file(path, path.name, path.parent.name)
+    print(f'Encrypted {path}')
+elif path:
+    print(f'{path} does not exist')
+else:
+    print('This script encrypts all decrypted datafiles from decrypted/*/*.json.')
+    print('This will overwrite all pre-existing datafiles saved in adventure/data.')
+    print('This is only necessary to edit the datafiles.')
+    print('IMPORTANT: Take extra caution not to commit decrypted datafiles.')
+    confirm = input('Reply with Y to confirm: ')
+
+    if confirm != 'Y':
+        exit(0)
+
+    for i in iglob('decrypted/*/*.json'):
+        theme = os.path.normpath(i).split(os.sep)[1]
+        name = os.path.split(i)[1]
+
+        encrpyt_file(i, name, theme)
+
+        print(name)
