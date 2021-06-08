7a67148514b654679e8feb8aa29f943959c5e4ca
Jia Rong Yee <28086837+fourjr@users.noreply.github.com>
2021-06-08 23:55:54 +0800

    ACC/RANT now

--- adventure/adventure.py
+++ adventure/adventure.py
@@ -3627,7 +3627,7 @@ class Adventure(MiscMixin, RoleMixin, commands.Cog):
             return
         c = await self.get_character_from_json(user)
 
-        legend = _("( RAGE | RANT | ACC | DEX | LUCK ) | LEVEL REQ | [DEGRADE#] | SET (SET PIECES)")
+        legend = _("( RAGE | ACC | RANT | DEX | LUCK ) | LEVEL REQ | [DEGRADE#] | SET (SET PIECES)")
         equipped_gear_msg = _("{user}'s Character Sheet\n\nItems Equipped:\n{legend}{equip}").format(
             legend=legend, equip=c.get_equipment(), user=c.user.display_name
         )
